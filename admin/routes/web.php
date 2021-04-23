d8253cc15ebb7205abeee62cacfa99a953072481
techrupt-devteam <developer@techrupt.in>
2021-04-23 12:28:23 +0530

    added clear route

--- admin/routes/web.php
+++ admin/routes/web.php
@@ -10,6 +10,13 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+Route::get('/clear', function() {
+	Artisan::call('cache:clear');
+	Artisan::call('config:clear');
+	Artisan::call('config:cache');
+	Artisan::call('view:clear');
+ 	return "Cleared!";
+ });
 
 
 
